1e9dfca760743d5d6667056e5c545e26e89f1fc6
Omer Selim AKBULUTLAR <omcell@hotmail.com>
2026-01-17 19:23:24 +0100

    Update

--- ada-html-0.2.0/main.js
+++
@@ -1,8 +0,0 @@
-import "./assets/css/tailus.css";
-
-const mainHeader = document.querySelector("#header");
-const menuBtn = document.querySelector("#menu-btn");
-
-menuBtn.addEventListener("click", () => {
-  mainHeader.dataset.state = mainHeader.dataset.state === "active" ? "closed" : "active";
-})
